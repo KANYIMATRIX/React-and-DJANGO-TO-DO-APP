64ab80f96b7900c836f87171bb4b68c6e05a0825
KANYIMATRIX <patrickkanyi321@gmail.com>
2019-11-09 16:09:39 +0300

    Add files via upload

---
+++ frontend/build/precache-manifest.906a8053a7f8ff47b0de266897ba15d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "757f7b4e35d71322b0bbaaa0bcd62ffa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a4742b25d35888c3417",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "b030ec14104d100fbffe",
+    "url": "/static/css/main.c8feef28.chunk.css"
+  },
+  {
+    "revision": "5a4742b25d35888c3417",
+    "url": "/static/js/2.66720c88.chunk.js"
+  },
+  {
+    "revision": "b030ec14104d100fbffe",
+    "url": "/static/js/main.f947fc0b.chunk.js"
+  },
+  {
+    "revision": "bb7e7463357611dc8d83",
+    "url": "/static/js/runtime-main.fca44c8b.js"
+  }
+]);
